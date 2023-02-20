0031300841288e059f7c5db470a9513a90c1ee51
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-02-20 23:33:00 +0300

    HW 3. Correction for error.

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,11 +27,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        if (onChangeChecked) {
-            onChangeChecked(e.currentTarget.checked)
-        }
-        // задачка на написание онченджа
-
+        if (onChangeChecked) onChangeChecked(e.currentTarget.checked)
+        if(onChange) onChange(e)
     }
 
     const finalInputClassName = s.checkbox
