4a06a4a68884013df303599cfb83eed287b8ec68
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-03-19 18:19:17 +0300

    HW 8. Made without styles.

---
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -0,0 +1,30 @@
+import {UserType} from '../HW8'
+
+type ActionType =
+    | { type: 'sort'; payload: 'up' | 'down' }
+    | { type: 'check'; payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+    switch (action.type) {
+        case 'sort': { // by name
+            return [ ...state.sort((a,b) => {
+                
+                // return action.payload === "up" ? a.name > b.name ?  1 : action.payload === "down" ? a.name < b.name ? 1 : -1
+                let value = 0
+                if ( action.payload === "up" ) {
+                    return value = a.name > b.name ?  1 : -1
+                }
+                if ( action.payload === "down" ) {
+                    return value = a.name < b.name ? 1 : -1
+                }
+                return value
+            })]
+        }
+        case 'check': {
+
+            return state.filter(s => s.age >= 18)
+        }
+        default:
+            return state
+    }
+}
